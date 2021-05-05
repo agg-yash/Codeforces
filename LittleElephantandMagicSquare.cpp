b2938516ccb56070f4109f79b2920f76dbfdc2d8
lemontree68 <83130251+lemontree68@users.noreply.github.com>
2021-05-05 12:41:06 +0530

    259-B Little Elephant and Magic Square

---
+++ LittleElephantandMagicSquare.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+
+    int x11, x12, x13, x21, x22, x23, x31, x32, x33;
+    cin >> x11 >> x12 >> x13 >> x21 >> x22 >> x23 >> x31 >> x32 >> x33;
+    int sum = (x12 + x13 + x21 + x23 + x31 + x32) / 2;
+    x11 = sum - x12 - x13;
+    x22 = sum - x21 - x23;
+    x33 = sum - x31 - x32;
+    cout << x11 << " " << x12 << " " << x13 << endl;
+    cout << x21 << " " << x22 << " " << x23 << endl;
+    cout << x31 << " " << x32 << " " << x33 << endl;
+
+
+	return 0;
+}
