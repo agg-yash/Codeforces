826e9c37dd91c54a0ac110185589bc6d700b844b
lemontree68 <83130251+lemontree68@users.noreply.github.com>
2021-05-10 11:14:56 +0530

    365-B The Fibonacci Segment

--- sol.cpp
+++ sol.cpp
@@ -18,18 +18,18 @@ using namespace std;
 #define PI 3.1415926535897932384626
 #define mod             1000000007
 #define inf             1e18
-typedef pair<int, int>  pii;
-typedef pair<ll, ll>  pl;
-typedef vector<int>    vi;
-typedef vector<ll>    vl;
-typedef vector<pii>    vpii;
-typedef vector<pl>    vpl;
-typedef vector<vi>    vvi;
-typedef vector<vl>    vvl;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
 mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
 int rng(int lim) {
-  uniform_int_distribution<int> uid(0, lim - 1);
-  return uid(rang);
+	uniform_int_distribution<int> uid(0, lim - 1);
+	return uid(rang);
 }
 
 
@@ -39,10 +39,10 @@ int rng(int lim) {
 
 void c_p_c()
 {
-  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
 }
 
@@ -51,45 +51,57 @@ void c_p_c()
 
 void solve() {
 
-  int i, j;
-  ll n, m; cin >> n >> m;
+	int i, j;
+	ll n;
+	cin >> n;
+	ll arr[n];
+	fo(i, n)
+	{
+		cin >> arr[i];
+	}
+	ll starti(0), endi(2);
+	if (n == 1 || n == 2)
+	{
+		cout << n;
+		return;
+	}
 
-  ll arr[n];
-  ll br[m];
-  fo(i, n)
-  {
-    cin >> arr[i];
-  }
-  fo(i, m)
-  {
-    cin >> br[i];
-  }
-  sort(arr, arr + n, greater<int>());
-  sort(br, br + m, greater<int>());
-  ll pos = 0;
-  ll ans = n;
-  for ( i = 0; i < n && pos < m  ; ++i)
-  {
-    if (arr[i] <= br[pos])
-    {
-      pos++;
-      ans--;
-    }
-  }
-  cout << ans;
+	ll ans = 2;
+
+	while (endi <= n - 1)
+	{
+		if (arr[endi] == arr[endi - 1] + arr[endi - 2] )
+		{
+			ans = max(ans, endi - starti + 1);
+			endi += 1;
+
+		}
+		else
+		{
+
+			starti = endi - 1;
+			endi += 1;
+		}
+
+	}
+	cout << ans;
+
+
+
+
 }
 
 int main() {
 
-  c_p_c();
+	c_p_c();
 
-  int t = 1;
+	int t = 1;
 
-  while (t--) {
-    solve();
-  }
+	while (t--) {
+		solve();
+	}
 
-  return 0;
+	return 0;
 }
 
 
